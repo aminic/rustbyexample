fb1a00c52b3d677b9f43e64ca00a0594b4252000
aminic <aminic.null@gmail.com>
2015-08-18 22:43:03 +0800

    generics

---
+++ generics.rs
@@ -0,0 +1,10 @@
+struct T;
+struct Single(T);
+struct SingleGen<T>(T);
+
+fn main() {
+    let _s = Single(T);
+    let _char: SingleGen < char >= SingleGen('a'); 
+    let _t = SingleGen(T); 
+    let _i32 = SingleGen(6);
+}
