a6ae7e3f3708995781dc125d6f316d35e54f3be6
aminic <aminic.null@gmail.com>
2015-08-19 17:53:33 +0800

    Mutablity

--- Mutability.rs
+++ Mutability.rs
@@ -5,4 +5,14 @@ fn main() {
     mutable_binding += 1;
     println!("After mutation: {}", mutable_binding);
     //_immutable_binding += 1;//error
+
+    println!("/////////////////////");
+    
+
+    let im_box = Box::new(5u32);
+    println!("immutable_box contains {}", im_box);
+    let mut mut_box = im_box;
+    println!("mutable_box contained {}", mut_box);
+    *mut_box = 4;
+    println!("mutable_box now contains {}", mut_box);
 }
